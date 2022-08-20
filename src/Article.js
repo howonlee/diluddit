128a2fa2ced061178ecec47a4537f77ca49a8efa
Howon Lee <hlee.howon@gmail.com>
2022-08-20 14:54:54 +0200

    lol

--- src/Article.js
+++ src/Article.js
@@ -1,6 +1,13 @@
-function Article({ id36 }) {
-  // list comments in article
-  return null;
+import { Outlet, useParams } from "react-router-dom";
+
+function ArticleParent() {
+  return <Outlet />;
+}
+
+function Article() {
+  let params = useParams();
+  return <p>"lol lol" {params.subredditName}</p>;
+  return <p>"lol lol" {params.id36}</p>;
 }
 
 export default Article;
