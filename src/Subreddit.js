f1247a719611d636c5358ed20dc8215e9d246804
Howon Lee <hlee.howon@gmail.com>
2022-08-20 09:25:33 +0200

    call it a sub

--- src/Subreddit.js
+++ src/Subreddit.js
@@ -1,6 +1,6 @@
-function Sub({ name count after }) {
+function Subreddit({ name count after }) {
   // whole sub listing component, dealing with reddit's pagination too
   return null;
 }
 
-export default Sub;
+export default Subreddit;
