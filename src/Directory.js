c1f39f9a42cb6f5a5c011bf3b49cb62ff7bfb2b8
Howon Lee <hlee.howon@gmail.com>
2022-08-20 08:02:06 +0200

    shove listing in...

--- src/Directory.js
+++ src/Directory.js
@@ -1,3 +1,46 @@
 // Directory of Subs.
 // There is no mixing of reddits.
 // The overall goal is to pretend it's 1999 and you're on a damn forum.
+//
+// TODO: un-hardcode listing. Maybe also don't shove like 70% of my sub preferences onto open internet
+
+let listing = [
+  "accounting",
+  "argentina",
+  "artisanvideos",
+  "askculinary",
+  "askhistorians",
+  "askmen",
+  "askprogramming",
+  "askreddit",
+  "askwomen",
+  "bestoflegaladvice",
+  "books",
+  "britishproblems",
+  "buttcoin",
+  "clojure",
+  "comics",
+  "construction",
+  "consulting",
+  "curatedtumblr",
+  "designdesign",
+  "experienceddevs",
+  "fire",
+  "gardening",
+  "historiansanswered",
+  "hobbydrama",
+  "kitchenconfidential",
+  "korea",
+  "machinelearning",
+  "medicine",
+  "mexico",
+  "nonpoliticaltwitter",
+  "programming",
+  "rebubble",
+  "sales",
+  "showerthoughts",
+  "specializedtools",
+  "subredditdrama",
+  "totalwar",
+  "warcollege"
+];
