4017199bf0e382160ae0c702c09532e409d29bc7
Howon Lee <hlee.howon@gmail.com>
2022-08-21 16:37:12 +0200

    thinking

--- src/Subreddit.js
+++ src/Subreddit.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import { Outlet, useParams } from "react-router-dom";
 
@@ -9,9 +9,37 @@ function SubredditParent() {
 
 // whole sub listing component, dealing with reddit's pagination too
 function Subreddit() {
-  let params = useParams();
-  let url = `https://www.reddit.com/r/${params.subredditName}.json`;
-  return <p>{url}</p>
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState("");
+  const [data, setData] = useState({});
+  const params = useParams();
+  const url = `https://www.reddit.com/r/${params.subredditName}.json`;
+
+  useEffect(() => {
+    const fetchData = async () => {
+      setLoading(true);
+      try {
+        const {data: newData} = await axios.get(url);
+        let children = newData?.data?.children;
+        let res = children.map((member) => member?.data?.title);
+        setData(res);
+      } catch (currError) {
+        setError(currError.message);
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchData();
+  }, []);
+  return (
+    <div>
+    {loading && <div>Loading...</div>}
+    {!loading && error && <div>Error: <span>{error}</span></div>}
+    {!loading && !error && (<div>
+      {data}
+        </div>)}
+    </div>
+  );
 }
 
 export { SubredditParent, Subreddit };
