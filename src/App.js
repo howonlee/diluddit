f05171a00a5bcc01db9278ba9fd82f393ac7da82
Howon Lee <hlee.howon@gmail.com>
2022-08-21 18:13:09 +0200

    back button works

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
-import { Outlet } from "react-router-dom";
+import { Link, Outlet, useNavigate } from "react-router-dom";
 
 function App() {
+  const nav = useNavigate();
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Diluddit</h1>
+        <h1><a href="#" onClick={() => nav(-1)}>&#x2190;</a></h1>
+        <h1><Link to="/">Diluddit</Link></h1>
       </header>
       <section className="App-section">
         <Outlet />
