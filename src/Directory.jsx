b02bf0123e59eb22cedb20f5154eab9ab53e9558
Howon Lee <hlee.howon@gmail.com>
2022-08-25 11:49:35 +0200

    kill hardcoded subs

--- src/Directory.jsx
+++ src/Directory.jsx
@@ -5,51 +5,10 @@
 // TODO: un-hardcode listing.
 // Maybe also don't shove like 70% of my sub preferences onto open internet
 
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import './Directory.css';
 import { DelayedLink } from './Utils';
-
-const listing = [
-  'accounting',
-  'argentina',
-  'artisanvideos',
-  'askculinary',
-  'askhistorians',
-  'askmen',
-  'askprogramming',
-  'askreddit',
-  'askwomen',
-  'bestoflegaladvice',
-  'books',
-  'britishproblems',
-  'buttcoin',
-  'clojure',
-  'comics',
-  'construction',
-  'consulting',
-  'curatedtumblr',
-  'designdesign',
-  'experienceddevs',
-  'fire',
-  'gardening',
-  'historiansanswered',
-  'hobbydrama',
-  'kitchenconfidential',
-  'korea',
-  'machinelearning',
-  'medicine',
-  'mexico',
-  'nonpoliticaltwitter',
-  'programming',
-  'rebubble',
-  'sales',
-  'showerthoughts',
-  'specializedtools',
-  'subredditdrama',
-  'totalwar',
-  'warcollege',
-];
 
 function DirectoryMember({ member }) {
   const url = `/r/${member}`;
@@ -65,8 +24,44 @@ DirectoryMember.propTypes = {
   member: PropTypes.string.isRequired,
 };
 
+function ListingAddForm({ dirData, setDirData }) {
+  const [name, setName] = useState('');
+  return (
+    <form
+      className="Listing-add-form"
+      onSubmit={(e) => {
+        dirData.listing.push(e.target[0].value);
+        setDirData(dirData);
+        window.localStorage.setItem('dirData', JSON.stringify(dirData));
+      }}
+    >
+      <input
+        type="text"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+        placeholder="Add sub name"
+        aria-label="subname"
+      />
+      <input
+        type="submit"
+        value="Submit"
+      />
+    </form>
+  );
+}
+
+ListingAddForm.propTypes = {
+  dirData: PropTypes.object.isRequired,
+  setDirData: PropTypes.func.isRequired,
+};
+
 function Directory() {
-  const listingDiv = listing.map(
+  const [dirData, setDirData] = useState(() => {
+    const saved = window.localStorage.getItem('dirData');
+    const init = JSON.parse(saved);
+    return init || { listing: [] };
+  });
+  const listingDiv = dirData.listing.map(
     (member) => (
       <div key={member} className="Directory-member">
         {' '}
@@ -76,6 +71,7 @@ function Directory() {
   );
   return (
     <div className="Directory">
+      <ListingAddForm dirData={dirData} setDirData={setDirData} />
       {listingDiv}
     </div>
   );
